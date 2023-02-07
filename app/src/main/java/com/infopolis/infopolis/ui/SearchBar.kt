9b71899570392570e58dcad0b941b46038880053
Thomas Roberson <thomas.roberson@bandlab.com>
2023-02-07 15:34:14 +0800

    - Cleaned up .gitignore

--- app/src/main/java/com/infopolis/infopolis/ui/SearchBar.kt
+++ app/src/main/java/com/infopolis/infopolis/ui/SearchBar.kt
@@ -1,5 +1,6 @@
 package com.infopolis.infopolis.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -11,12 +12,14 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
@@ -24,8 +27,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+fun test_TextSearch(input: String): Unit {}
+@Preview
+@Composable
+fun SearchViewPreview() {
+    SearchBar(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(10.dp)
+            .fillMaxWidth(),
+        onChange = {test_TextSearch("")},
+        input = "Hello"
+    )
+}
 @Composable
 fun SearchInput(
     onInput: () -> String,
@@ -33,18 +50,18 @@ fun SearchInput(
     hint: (FocusState) -> Unit,
 ) {
     BasicTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged {
+                hint(it)
+            }
+            .padding(horizontal = 10.dp),
         value = onInput(),
         onValueChange = onChange,
         maxLines = 1,
         singleLine = true,
         cursorBrush = SolidColor(Color.White),
         textStyle = TextStyle(color = Color.White, textAlign = TextAlign.Left),
-        modifier = Modifier
-            .fillMaxWidth()
-            .onFocusChanged {
-                hint(it)
-            }
-            .padding(vertical = 12.dp)
     )
 }
 @Composable
@@ -55,6 +72,7 @@ fun SearchBar(
 ) {
     Row(
         horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
     ) {
 
@@ -63,12 +81,13 @@ fun SearchBar(
             Icons.Default.Search,
             contentDescription = "Search icon",
             modifier = Modifier
-                .width(50.dp)
-                .height(20.dp),
+                .width(20.dp)
+                .height(20.dp)
+                ,
             tint = Color.White,
         )
 
-        Box(modifier = modifier) {
+        Box {
             SearchInput(
                 onInput = { input },
                 onChange = onChange,
